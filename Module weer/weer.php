883ada52b9404f937e10cf7e20c54fc9afaf5711
Bram Meeuwissen <bram.meeuwissen@gmail.com>
2021-06-17 11:15:12 +0200

    module weer en poster technisch

---
+++ Module weer/weer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "Bram" ?> </title>
+</head>
+<body>
+    <iframe src="//gadgets.buienradar.nl/gadget/forecastandstation/6370/" noresize scrolling=no hspace=0 vspace=0 frameborder=0 marginheight=0 marginwidth=0 width=300 height=190></iframe>
+</body>
+</html>
