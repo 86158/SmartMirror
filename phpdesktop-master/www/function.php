55e29ca9b2e9e79c5587dcee15a4e6f63da48935
Steven <86513@roc-teraa.nl>
2021-06-24 10:15:22 +0200

    de smartmirrror en een phpwebserver die zo werkt

---
+++ phpdesktop-master/www/function.php
@@ -0,0 +1,78 @@
+<?php
+function db_connect(){
+ $servername    ="localhost";
+ $username      ="root";
+ $password  	="";
+ $dbname        ="smartmirror";
+
+ $conn = new mysqli($servername, $username, $password, $dbname);
+
+ if($conn->connect_error)
+ {
+     die("Connection failed: ". $conn->connect_error);
+ }
+ return $conn;
+}
+
+function randomZin(){
+    $conn = db_connect();
+
+    $sql = "SELECT * FROM `randomzin` WHERE `zinId` ORDER BY RAND() LIMIT 1";
+    $result = $conn->query($sql) or die($conn->error);
+    $posts = $result->fetch_all(MYSQLI_ASSOC);
+
+
+    $randZinnen = [];
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) 
+            {
+            $randZinnen[] = $row;
+            }
+        }
+    else {
+        print($posts);
+    }
+    return $posts;
+    $result = $conn->query($sql);
+}
+function Date_show()
+{
+  $WeekDay = array(
+    "Zondag",
+    "Maandag",
+    "Dinsdag",
+    "Woensdag",
+    "Donderdag",
+    "Vrijdag",
+    "Zaterdag"
+);
+$dayOfWeek = $WeekDay[date("w")];
+$month = array(
+    "Januari",
+    "Februari",
+    "Maart",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Augustus",
+    "September",
+    "Oktober",
+    "November",
+    "December"
+);
+$date = date("d") . " " . $month[date("n") - 1] . " " . date("Y");
+echo $dayOfWeek . " " . $date;
+}
+//Get the website url for the javascript auto refresh
+function getMyUrl()
+{
+  $protocol = (!empty($_SERVER['HTTPS']) && (strtolower($_SERVER['HTTPS']) == 'on' || $_SERVER['HTTPS'] == '1')) ? 'https://' : 'http://';
+  $server = $_SERVER['SERVER_NAME'];
+  $port = $_SERVER['SERVER_PORT'] ? ':'.$_SERVER['SERVER_PORT'] : '';
+  return $protocol.$server.$port;
+}
+
+?>
---
+++ phpdesktop-master/www/roosterExport.php
@@ -0,0 +1,124 @@
+<?php
+ $roosterArray = array (
+	0 => array (
+		'day' => 'MAANDAG 21 JUNI',
+		'subjects' => array (
+			0 => array (
+				'vak' => 'Online les IC - enge - IC.20SD.a - BITA',
+				'begintijd' => '10:15',
+				'eindtijd' => '10:45',
+			),
+			1 => array (
+				'vak' => 'Online les IC - enge - IC.20SD.a - BITA',
+				'begintijd' => '11:00',
+				'eindtijd' => '11:30',
+			),
+			2 => array (
+				'vak' => 'Online les IC - prta - IC.20SD.a - DORI',
+				'begintijd' => '11:30',
+				'eindtijd' => '12:30',
+			),
+			3 => array (
+				'vak' => 'K1.44,k1.42b,K1.42a - vrar - IC.K0781.20C - SPTH',
+				'begintijd' => '13:00',
+				'eindtijd' => '14:30',
+			),
+			4 => array (
+				'vak' => 'K1.44,k1.42b,K1.42a - vrar - IC.K0781.20C - SPTH',
+				'begintijd' => '14:45',
+				'eindtijd' => '16:15',
+			),
+		),
+	),
+	1 => array (
+		'day' => 'DINSDAG 22 JUNI',
+		'subjects' => array (
+			0 => array (
+				'vak' => 'K1.42a - burg - IC.20SD.a - FOIR',
+				'begintijd' => '8:45',
+				'eindtijd' => '10:15',
+			),
+			1 => array (
+				'vak' => 'K1.42a - prta - IC.20SD.a - DORI',
+				'begintijd' => '10:15',
+				'eindtijd' => '10:45',
+			),
+			2 => array (
+				'vak' => 'K1.42a - prta - IC.20SD.a - DORI',
+				'begintijd' => '11:00',
+				'eindtijd' => '12:30',
+			),
+			3 => array (
+				'vak' => 'K1.42a - prta - IC.20SD.a - DORI',
+				'begintijd' => '13:00',
+				'eindtijd' => '14:30',
+			),
+		),
+	),
+	2 => array (
+		'day' => 'WOENSDAG 23 JUNI',
+		'subjects' => array (
+			0 => array (
+				'vak' => 'Online les IC - ites - IC.20SD.a - LOCE',
+				'begintijd' => '8:45',
+				'eindtijd' => '10:15',
+			),
+			1 => array (
+				'vak' => 'Online les IC - prta - IC.20SD.e,IC.20SD.b,IC.20SD.a - GEST',
+				'begintijd' => '10:15',
+				'eindtijd' => '10:45',
+			),
+			2 => array (
+				'vak' => 'Online les IC - nede - IC.20SD.a - DKEL',
+				'begintijd' => '11:00',
+				'eindtijd' => '12:30',
+			),
+			3 => array (
+				'vak' => 'Online les IC - prta - IC.20SD.a - DORI',
+				'begintijd' => '13:00',
+				'eindtijd' => '14:30',
+			),
+		),
+	),
+	3 => array (
+		'day' => 'DONDERDAG 24 JUNI',
+		'subjects' => array (
+			0 => array (
+				'vak' => 'K1.42a - prta - IC.20SD.a - NOPE',
+				'begintijd' => '8:45',
+				'eindtijd' => '9:45',
+			),
+			1 => array (
+				'vak' => 'K1.42a - reke - IC.20SD.a - RIHA',
+				'begintijd' => '9:45',
+				'eindtijd' => '10:45',
+			),
+			2 => array (
+				'vak' => 'K1.42a - prta - IC.20SD.a - NOPE',
+				'begintijd' => '11:00',
+				'eindtijd' => '12:30',
+			),
+		),
+	),
+	4 => array (
+		'day' => 'VRIJDAG 25 JUNI',
+		'subjects' => array (
+			0 => array (
+				'vak' => 'K1.42a - prta - IC.20SD.a - POBE',
+				'begintijd' => '8:45',
+				'eindtijd' => '10:45',
+			),
+			1 => array (
+				'vak' => 'K1.42a - prta - IC.20SD.a - POBE',
+				'begintijd' => '11:00',
+				'eindtijd' => '12:30',
+			),
+			2 => array (
+				'vak' => 'K1.42a - prta - IC.20SD.a - POBE,NOPE',
+				'begintijd' => '13:00',
+				'eindtijd' => '14:30',
+			),
+		),
+	),
+)
+?>
