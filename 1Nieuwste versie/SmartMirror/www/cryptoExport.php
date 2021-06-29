bfcc3a61b907d2191e306bea4cb899f9501fab73
Steven <86513@roc-teraa.nl>
2021-06-29 14:11:17 +0200

    Het compilde installer met alle bestanden op de goede plek
    HOUD alles in deze folder. open de shortcut voor de installatie en hierin kan ook dus nog het python script aangepast worden en ook de website. Zorg dat zodra alles aangepast is de website de "rar-t" wordt zodat het programma het correct kan installeren

---
+++ 1Nieuwste versie/SmartMirror/www/cryptoExport.php
@@ -0,0 +1,28 @@
+<?php
+ $cryptoArray =  array (
+	1 => array (
+		'naam' => '  Bitcoin',
+		'desc' => '$35.616,29 +4,61% $35.558.715.696',
+	),
+	2 => array (
+		'naam' => '  Ethereum',
+		'desc' => '$2.177,86 +9,12% $25.936.104.014',
+	),
+	3 => array (
+		'naam' => '  Tether',
+		'desc' => '$1,00 +0,06% $61.030.540.643',
+	),
+	4 => array (
+		'naam' => '  Binance Coin',
+		'desc' => '$307,98 +7,98% $1.783.793.998',
+	),
+	5 => array (
+		'naam' => '  Cardano',
+		'desc' => '$1,39 +5,90% $2.634.290.771',
+	),
+	6 => array (
+		'naam' => '  Dogecoin',
+		'desc' => '$0,27 +6,57% $1.856.964.932',
+	),
+ )
+?>
---
+++ 1Nieuwste versie/SmartMirror/www/feestdagExport.php
@@ -0,0 +1,20 @@
+<?php
+ $feestdagenArray =  array (
+	0 => array (
+		'datum' => '26 December 2021',
+		'desc' => '2e Kerstdag 2021',
+	),
+	1 => array (
+		'datum' => '01 Januari 2022',
+		'desc' => 'Nieuwjaarsdag 2022',
+	),
+	2 => array (
+		'datum' => '15 April 2022',
+		'desc' => 'Goede vrijdag 2022',
+	),
+	3 => array (
+		'datum' => '17 April 2022',
+		'desc' => '1e Paasdag 2022',
+	),
+ )
+?>
